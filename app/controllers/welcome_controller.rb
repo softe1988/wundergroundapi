1eda785fc628d81d1d2f89dd288e9ffb5c230182
Simone Battiste-Alleyne <softe1988@gmail.com>
2015-06-10 05:06:37 +0000

    added bootstrap

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,19 +1,8 @@
 class WelcomeController < ApplicationController
-  # require 'httparty'
+  
   def index
-  # open('http://api.wunderground.com/api/54a3f0a74c50658c/geolookup/conditions/q/GA/Atlanta.json') do |f|
-		# 	  json_string = f.read
-		# 	 	parsed_json = JSON.parse(json_string)
-		# 	  @location = parsed_json['location']['city']
-		# 	  @temp_f = parsed_json['current_observation']['temp_f']
-		# 	  @temp_c = parsed_json['current_observation']['temp_c']
-		# 	  @icon = parsed_json['current_observation']['icon_url']
-		# 	  @in_words = parsed_json['current_observation']['weather']
-		# 	  @forecast_link = parsed_json['current_observation']['forecast_url']
-		# 	  @real_feel = parsed_json['current_observation']['feelslike_f']
 	end
  
-
   def test
   response = HTTParty.get("http://api.wunderground.com/api/dc525e7ffa52770b/geolookup/conditions/q/TX/Dallas.json")
   
