c5ae50fdb18cfd3f92efc4bbe4350f4e23db4df7
Simone Battiste-Alleyne <softe1988@gmail.com>
2015-06-12 16:58:21 +0000

    search functionality works need to fix weather icon

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,54 @@
 class WelcomeController < ApplicationController
   
+	require 'open-uri'
+	require 'json'
+  
   def index
-	end
+# 	@states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC)
+	
+# 	@states.sort!
+	
+# 	response = HTTParty.get("http://api.wunderground.com/api/dc525e7ffa52770b/geolookup/conditions/q/#{params[:state]}/#{params[:city]}.json")
+	
+#   @location = response['city']['state']
+#   @temp_f = response['current_observation']['temp_f']
+#   @temp_c = response['current_observation']['temp_c']
+#   @weather_icon = response['current_observation']['icon_url']
+#   @weather_words = response['current_observation']['weather'] 
+#   @forecast_link = response['current_observation']['forecast_url']
+#   @real_feel = response['current_observation']['feelslike_f']
+  @states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC)
+  	@states.sort!
+
+  	if params[:city] != nil
+  		if params[:city] != ""
+	  		if params[:city].include?(" ")
+	  			params[:city].gsub! " ", "_"
+	  		end
+
+	  		if params[:state] == ""
+	  			url = "http://api.wunderground.com/api/dc525e7ffa52770b/geolookup/conditions/q/#{params[:country]}/#{params[:city]}.json"
+	  		else
+	  			url = "http://api.wunderground.com/api/dc525e7ffa52770b/geolookup/conditions/q/#{params[:state]}/#{params[:city]}.json"
+	  		end
+
+
+	  		open(url) do |f|
+	  			json_string = f.read
+				 	parsed_json = JSON.parse(json_string)
+				  @location = parsed_json['location']['city']
+				  @temp_f = parsed_json['current_observation']['temp_f']
+				  @temp_c = parsed_json['current_observation']['temp_c']
+				  @icon = parsed_json['current_observation']['icon_url']
+				  @in_words = parsed_json['current_observation']['weather']
+				  @forecast_link = parsed_json['current_observation']['forecast_url']
+				  @real_feel = parsed_json['current_observation']['feelslike_f']
+	  		end
+  end
+  elsif params[:city] == "" && params[:button_pushed] == true
+  		redirect_to find_weather_path
+  		
+  	end
  
   def test
   response = HTTParty.get("http://api.wunderground.com/api/dc525e7ffa52770b/geolookup/conditions/q/TX/Dallas.json")
@@ -13,5 +60,6 @@ class WelcomeController < ApplicationController
   @weather_words = response['current_observation']['weather'] 
   @forecast_link = response['current_observation']['forecast_url']
   @real_feel = response['current_observation']['feelslike_f']
+  end
   end
 end
